edc74215316943e01a36675f0290104c1f4b9b63
am-kh <khalil-am@outlook.com>
2024-11-17 03:22:32 +0300

    +bug fixes

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -21,7 +21,7 @@ export async function getPostBySlug(
 ): Promise<Post | null> {
   try {
     const filePath = path.join(rootDirectory, `${slug}.mdx`);
-    const fileContents = await fs.readFileSync(filePath, { encoding: "utf-8" });
+    const fileContents = await fs.promises.readFile(filePath, { encoding: "utf-8" });
     const { data, content } = matter(fileContents);
 
     return { metadata: { ...data, slug }, content };
@@ -34,16 +34,19 @@ export async function getPosts(
   rootDirectory: string,
   limit?: number,
 ): Promise<PostMetadata[]> {
-  const files = fs.readdirSync(rootDirectory);
+  const files = await fs.promises.readdir(rootDirectory);
 
-  const posts = files
-    .filter((file) => file.endsWith(".mdx")) // might have .DS_Store?
-    .map((file) => getPostMetaData(rootDirectory, file))
-    .sort(
-      (a, b) =>
-        (new Date(b.publishedAt ?? "").getTime() || 0) -
-        (new Date(a.publishedAt ?? "").getTime() || 0),
-    );
+  const posts = await Promise.all(
+    files
+      .filter((file) => file.endsWith(".mdx")) // might have .DS_Store?
+      .map((file) => getPostMetaData(rootDirectory, file))
+  );
+
+  posts.sort(
+    (a, b) =>
+      (new Date(b.publishedAt ?? "").getTime() || 0) -
+      (new Date(a.publishedAt ?? "").getTime() || 0),
+  );
 
   if (limit) {
     return posts.slice(0, limit);
@@ -52,13 +55,13 @@ export async function getPosts(
   return posts;
 }
 
-export function getPostMetaData(
+export async function getPostMetaData(
   rootDirectory: string,
   filePath: string,
-): PostMetadata {
+): Promise<PostMetadata> {
   const slug = filePath.replace(/\.mdx$/, "");
   const fullFilePath = path.join(rootDirectory, filePath);
-  const fileContent = fs.readFileSync(fullFilePath, { encoding: "utf8" });
+  const fileContent = await fs.promises.readFile(fullFilePath, { encoding: "utf8" });
   const { data } = matter(fileContent);
 
   return { ...data, slug };
