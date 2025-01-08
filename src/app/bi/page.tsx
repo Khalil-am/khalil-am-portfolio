e102d637b0e2b1ecec563c92e539351bc053058c
Khalil-am <khalil-am@outlook.com>
2025-01-08 14:44:01 +0300

    + introducing bugs

--- src/app/bi/page.tsx
+++ src/app/bi/page.tsx
@@ -5,7 +5,9 @@ export default async function BIPage() {
   return (
     <article className="mt-8 flex flex-col gap-8 pb-16">
       <h1 className="title">my BI Dashboards</h1>
-      <BI />
+      {/*<BI />*/}
+      <h1 className="-body">Building Dashboard visualization tool for both Power BI & Tableau (Coming soon!)</h1>
+
     </article>
   );
 }
